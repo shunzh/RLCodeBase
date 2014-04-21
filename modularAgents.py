0c27347c6dab9ef85057583894967e6e6698ed7e
Shun Zhang <menie482@gmail.com>
2014-04-20 20:23:05 -0500

    implemented modular md

--- modularAgents.py
+++ modularAgents.py
@@ -27,19 +27,26 @@ def getObsAvoidFuncs(mdp):
 	obstacle = {'bias': -0.20931133310480204, 'dis': 0.06742681562641269}
 	sidewalk = {'x': 0.06250000371801567}
 
-	def qFuncs(state, action):
-		rets = []
-
+	def qValues(state, action):
 		x, y = state
 		dx, dy = Actions.directionToVector(action)
 		next_x, next_y = int(x + dx), int(y + dy)
 
 		# forward walking
 		qWalk = sidewalk['x'] * next_x
-		rets.append(qWalk)
 
 		# obstacle avoiding
-		# punish it if it's a obstacle
+		# find the distance to the nearest obstacle
+		minDist = mdp.grid.width * mdp.grid.height
+		for xt in range(mdp.grid.width):
+			for yt in range(mdp.grid.height):
+				cell = self.grid[xt][yt] 
+				if (type(cell) == int or type(cell) == float) and cell < 0:
+					# it's an obstacle!
+					dist = math.sqrt((xt - next_x) ** 2 + (yt - next_y) ** 2)
+					if (dist < minDist): minDist = dist
+		qObstacle = minDist * obstacle['dis'] + 1 * obstacle['bias']
 
-	
-	return qFuncs
+		return [qWalk, qObstacle]
+
+	return qValues
