f6828014192124304f6b564fdc7a0678306a07e1
Shun Zhang <menie482@gmail.com>
2015-06-27 12:19:00 -0500

    cma-es not working in unittest. weird

--- inverseModularRLTest.py
+++ inverseModularRLTest.py
@@ -4,17 +4,17 @@ Try some simple domains, and human world experiments on both discrete and contin
 """
 import unittest
 from inverseModularRL import InverseModularRL
-import inverseModularRLHumanExperiments
 
 class Test(unittest.TestCase):
+  methodCandidates = ["CMA-ES", "BFGS"]
   def test_corridor(self):
     """
     Two objectives - going to left or going to right in a corridor.
     """
     actions = ['L', 'R']
     samples = [[(p, actions[0]) for p in range(2)], [(p, actions[1]) for p in range(2)]]
-    qFuncs = [lambda s, a, d = None: 1 if a == actions[0] else 0,\
-              lambda s, a, d = None: 1 if a == actions[1] else 0]
+    qFuncs = [lambda s, a, x = None: x[0] if a == actions[0] else 0,\
+              lambda s, a, x = None: x[1] if a == actions[1] else 0]
     resultConstraints = [lambda w: w[0] >= 0.99 and w[1] <= 0.01,\
                          lambda w: w[1] >= 0.99 and w[0] <= 0.01]
     self.checkResult(samples, actions, qFuncs, resultConstraints)
@@ -26,9 +26,9 @@ class Test(unittest.TestCase):
     actions = range(3)
 
     samples = [[(0, action)] for action in actions]
-    qFuncs = [lambda s, a, d = None: 1 if a == actions[0] else 0,\
-              lambda s, a, d = None: 1 if a == actions[1] else 0,\
-              lambda s, a, d = None: 1 if a == actions[2] else 0]
+    qFuncs = [lambda s, a, x = None: x[0] if a == actions[0] else 0,\
+              lambda s, a, x = None: x[1] if a == actions[1] else 0,\
+              lambda s, a, x = None: x[2] if a == actions[2] else 0]
     resultConstraints = [lambda w: w[0] >= 0.99,\
                          lambda w: w[1] >= 0.99,\
                          lambda w: w[2] >= 0.99]
@@ -38,20 +38,23 @@ class Test(unittest.TestCase):
     n = len(qFuncs)
 
     for expIdx in range(len(samples)):
-      sln = InverseModularRL(qFuncs)
-      sln.getSamples = lambda: samples[expIdx]
-      sln.getActions = lambda s: actions
-      x = sln.solve()
-      w = x[:n]
-      d = x[n:]
+      for methodCandidate in Test.methodCandidates: 
+        starts = [0] * n
+        bnds = tuple((0, 1000) for _ in xrange(n))
 
-      judge = resultConstraints[expIdx](x)
-      if judge == False:
-        # plot weights upon failure
-        inverseModularRLHumanExperiments.printWeight(sln, unittest.TestCase.id(self) + '_' + str(expIdx) + '_w.png', d)
-        inverseModularRLHumanExperiments.printDiscounter(sln, unittest.TestCase.id(self) + '_' + str(expIdx) + '_d.png', w)
+        sln = InverseModularRL(qFuncs, starts, bnds, solver=methodCandidate)
 
-      self.assertTrue(judge, msg="Exp #" + str(expIdx) + " weights: " + str(x))
+        sln.getSamples = lambda: samples[expIdx]
+        sln.getActions = lambda s: actions
+        x = sln.solve()
+
+        w = x[:n]
+        sumW = sum(w)
+        w = [wi / sumW for wi in w]
+
+        judge = resultConstraints[expIdx](w)
+
+        self.assertTrue(judge, msg="Exp #" + str(expIdx) + " parameter: " + str(w) + " with " + methodCandidate)
 
 if __name__ == '__main__':
   unittest.main()
