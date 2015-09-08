c6e0fc81d4bf1f93c06d4a70713f3f53ad467d6f
Shun Zhang <menie482@gmail.com>
2015-09-08 00:52:52 -0400

    some more imp in jqtp

--- JQTPAgent.py
+++ JQTPAgent.py
@@ -1,16 +1,17 @@
 from learningAgents import ReinforcementAgent
 from valueIterationAgents import ValueIterationAgent
 import util
+import copy
 
 class JPQTAgent:
-  def __init__(self, cmp, rewardSet, gamma=0.9):
+  def __init__(self, cmp, rewardSet, initialPhi=None, gamma=0.9):
     self.cmp = cmp
     self.rewardSet = rewardSet
     self.gamma = gamma
     
     self.n = self.cmp.n
     # init belief on rewards with uniform distribution
-    self.phi = [1.0 / self.n] * self.n
+    self.phi = initialPhi or [1.0 / self.n] * self.n
 
   def getRewardFunc(self, state, phi):
     #TODO
@@ -21,12 +22,12 @@ class JPQTAgent:
     """
     #TODO
 
-  def getOptValue(self, state, phi):
+  def getOptValues(self, phi):
     cmp = copy.deepcopy(self.cmp)
     cmp.rewardFunc = self.getRewardFunc(phi)
 
     viAgent = ValueIterationAgent(cmp, discount=self.gamma)
-    return viAgent.getValue(state)
+    return viAgent.getValues()
   
   def getValue(self, state, phi, pi, horizon):
     sum = 0
@@ -37,7 +38,8 @@ class JPQTAgent:
     
     return sum
 
-  def getBeliefProb(self):
+  def getPossiblePhiAndProbs(self):
+    #TODO
     pass
 
   def getQValue(self, state, policy, query):
@@ -45,10 +47,33 @@ class JPQTAgent:
 
     responseTime = self.cmp.getResponseTime(state)
     vBeforeResponse = self.getValue(state, self.phi, policy, responseTime)
-    vAfterResponse = self.gamma ** responseTime * #TODO
     
-    return cost + vBeforeResponse
+    possiblePhis = self.getPossiblePhiAndProbs()
+    possibleStatesAndProbs = getMultipleTransitionMatrix(cmp, policy, responseTime)
+    
+    vAfterResponse = 0
+    for fPhi, fPhiProb in possiblePhis:
+      values = self.getOptValues(fPhi)
+      estimatedValue = 0
+      for fState, fStateProb in possibleStatesAndProbs:
+        estimatedValue += values(fState) * fStateProb
+      vAfterResponse += fPhiProb * estimatedValue
+
+    vAfterResponse *= self.gamma ** responseTime
+
+    return cost + vBeforeResponse + vAfterResponse
   
   def learn(self):
     # iterate optimize over policy and query
     #TODO
+
+def getTransitionMatrix(cmp, policy):
+  #TODO
+  pass
+
+def getMultipleTransitionMatrix(cmp, policy, time):
+  """
+  Multiply a transition matrix multiple times
+  """
+  #TODO
+  pass
