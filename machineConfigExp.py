8b3fa1b5c1692fd7f5b90779a79af407b02e3750
Shun Zhang <menie482@gmail.com>
2015-09-11 00:57:34 -0400

    match rob's paper's experiment

--- machineConfigExp.py
+++ machineConfigExp.py
@@ -1,19 +1,24 @@
 from machineConfig import MachineConfiguration
 from JQTPAgent import JQTPAgent
+import random
 
 def main():
   numMachines = 3
   numConfigs = 3
+  rewardNum = 10
 
-  factoredReward = lambda i, j: j
-  alterFactoredReward = lambda i, j: 3 - j
-  rewardSet = map(lambda rf: rewardFuncGen(rf, numMachines), [factoredReward, alterFactoredReward])
+  factoredRewards = []
+  randomTable = [random.random() * 3 for _ in xrange(numMachines * (numConfigs+1) * rewardNum)]
+  for idx in range(rewardNum):
+    rewardFunc = lambda i, j: randomTable[idx * numMachines * numConfigs + i * numMachines + j - 1]
+    factoredRewards.append(rewardFunc)
+  rewardSet = map(lambda rf: rewardFuncGen(rf, numMachines), factoredRewards)
 
   # can ask a configuration of a machine
   queries = [(0, 1, 1), (1, 0, 1), (1, 1, 0)]
   cmp = MachineConfiguration(numMachines, numConfigs, rewardSet[0], queries)
   
-  initialPhi = [.5, .5]
+  initialPhi = [1.0 / rewardNum] * rewardNum
   agent = JQTPAgent(cmp, rewardSet, initialPhi)
   
   agent.learn()
