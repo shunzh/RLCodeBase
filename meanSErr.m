d70c9b22ed59bd94b819575ae043319b1d325113
Shun Zhang <shunzh@caen-vnc20.engin.umich.edu>
2015-10-03 16:50:36 -0400

    modified m file

--- meanSErr.m
+++ meanSErr.m
@@ -1,11 +1,16 @@
-arg_list = argv();
-filename = ["results", arg_list{1}];
+load('results');
+load('beliefs');
+load('time');
 
-results = load(filename);
-
-disp("number of data")
+disp('number of data')
 size(results, 1)
 
-disp("results")
+disp('results')
 sprintf('%.3f', mean(results))
 sprintf('%.3f', 1.96 * std(results) / size(results, 1))
+
+disp('beliefs')
+sprintf('%.3f', mean(beliefs))
+
+disp('time')
+sprintf('%.3f', mean(time))
