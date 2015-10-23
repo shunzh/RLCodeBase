e7d03ca94b23a13faf5326fa2e1a73a769954d72
Shun Zhang <menie482@gmail.com>
2015-10-23 10:41:06 -0500

    update m and condor file

--- meanSErr.m
+++ meanSErr.m
@@ -7,10 +7,8 @@ size(results, 1)
 
 disp('results')
 sprintf('%.3f', mean(results))
-sprintf('%.3f', 1.96 * std(results) / size(results, 1))
-
-disp('beliefs')
-sprintf('%.3f', mean(beliefs))
+sprintf('%.3f', 1.96 * std(results) / sqrt(size(results, 1)))
 
 disp('time')
 sprintf('%.3f', mean(time))
+sprintf('%.3f', 1.96 * std(time) / sqrt(size(time, 1)))
