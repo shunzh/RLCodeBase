b7fedd94a38cea6787f8c9fd0882e108df32ce79
Shun Zhang <menie482@gmail.com>
2015-12-08 11:46:23 -0500

    change on plotting function

--- plotNWResults.m
+++ plotNWResults.m
@@ -4,53 +4,56 @@ function main()
 
   clear all; close all;
 
-  responseTimes = [6, 10, 20];
+  corridorNums = [1, 2, 4];
   filename = 'nw_out';
 
   retMat = 1:3:200;
   jMat = retMat(1:3);
   aMat = retMat(4:6);
-  anfMat = retMat(7:9);
+  %anfMat = retMat(7:9);
 
   [jm, jc] = process(filename, jMat + 1)
   [am, ac] = process(filename, aMat + 1)
-  [anm, anc] = process(filename, anfMat + 1)
+  %[anm, anc] = process(filename, anfMat + 1)
 
-  errorbar(responseTimes, jm, jc, '*-');
+  errorbar(corridorNums, jm, jc, '*-');
   hold on;
-  errorbar(responseTimes, am, ac, 'o-');
-  errorbar(responseTimes, anm, anc, 'o--');
+  errorbar(corridorNums, am, ac, 'o-');
+  %errorbar(responseTimes, anm, anc, 'o--');
 
-  legend('E-JQTP', 'AQTP-QF', 'AQTP-NQF');
-  xlabel('Response Time');
+  legend('E-JQTP', 'AQTP-NQF');
+  xlabel('Number of Corridors');
   ylabel('Q-Value');
-  xlim([-1, 25]); 
+  xlim([0, 5]); 
   
   figure;
 
   [jm, jc] = process(filename, jMat + 2)
   [am, ac] = process(filename, aMat + 2)
-  [anm, anc] = process(filename, anfMat + 2)
+  %[anm, anc] = process(filename, anfMat + 2)
 
-  errorbar(responseTimes, jm, jc, '*-');
+  errorbar(corridorNums, jm, jc, '*-');
   hold on;
-  errorbar(responseTimes, am, ac, 'o-');
-  errorbar(responseTimes, anm, anc, 'o--');
+  errorbar(corridorNums, am, ac, 'o-');
+  %errorbar(responseTimes, anm, anc, 'o--');
 
-  legend('E-JQTP', 'AQTP-QF', 'AQTP-NQF');
-  xlabel('Response Time');
+  legend('E-JQTP', 'AQTP-NQF');
+  xlabel('Number of Corridors');
   ylabel('Computation Time (sec.)');
-  xlim([-1, 25]); 
+  xlim([-1, 5]); 
 
-  figure;
-
-  jd = getData(filename, [jMat(3)] + 1);
-  ad = getData(filename, [aMat(3)] + 1);
-  histogram(jd - ad, 'FaceColor', [.8, .8, .8]);
-  hold on
-  plot(0, sum(jd - ad == 0), '*k');
-  xlabel('Difference in Q value between E-JQTP and AQTP-QF');
-  ylabel('Frequency');
+  for i = 1:3
+    figure;
+    jd = getData(filename, [jMat(i)] + 1);
+    ad = getData(filename, [aMat(i)] + 1);
+    histogram(jd - ad, 'FaceColor', [.8, .8, .8]);
+    hold on
+    plot(0, sum(jd - ad == 0), '*k');
+    xlabel('Difference in Q value between E-JQTP and AQTP-NQF');
+    ylabel('Frequency');
+    title(['Number of Corridors ', num2str(corridorNums(i))]);
+    ylim([0, 200]); 
+  end
 end
 
 function data = getData(filename, lines)
@@ -58,7 +61,7 @@ function data = getData(filename, lines)
   for i=0:199
     try
       raw = load([filename, '.', num2str(i)]);
-      if size(raw, 1) == 27
+      if size(raw, 1) == 18
         data = [data, raw(lines)];
       end
     catch
