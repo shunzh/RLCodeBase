ef74a3e69c81a705060b892037145cf9cf2b4d6a
Shun Zhang <menie482@gmail.com>
2016-06-22 15:26:05 -0400

    finished imp, debug

--- easyDomains.py
+++ easyDomains.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-def getRockDomain(size, numRocks, rewardCandNum, randSeed = 0):
+def getRockDomain(size, numRocks, rewardCandNum, fixedRocks = False, randSeed = 0):
   """
   Return a domain with a number of random rocks.
 
@@ -11,7 +11,7 @@ def getRockDomain(size, numRocks, rewardCandNum, randSeed = 0):
   ret = {}
 
   ret['S'] = [(x, y) for x in xrange(size) for y in xrange(size)]
-  ret['A'] = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+  ret['A'] = [(-1, 1), (0, 1), (1, 1)]
   def transition(s, a):
     loc = [s[0] + a[0], s[1] + a[1]]
 
@@ -25,11 +25,22 @@ def getRockDomain(size, numRocks, rewardCandNum, randSeed = 0):
   ret['T'] = transition
 
   ret['R'] = []
-  for _ in xrange(rewardCandNum):
-    # select rocks randomly from S
-    rocks = np.random.permutation(S)[:numRocks]
-    r = lambda s: 1 if s in rocks else 0
-    ret['R'].append(r)
+  if fixedRocks:
+    possibleRocks = [(0, size - 1),\
+                     ((size - 1) / 3, size - 1),\
+                     ((size - 1) * 2 / 3, size - 1),\
+                     (size - 1, size - 1)]
+    for i in xrange(4):
+      r = lambda s, a: 1 if s == possibleRocks[i] else 0
+      ret['R'].append(r)
+  else:
+    for _ in xrange(rewardCandNum):
+      # select rocks randomly from S
+      rocks = np.random.permutation(ret['S'])[:numRocks]
+      r = lambda s, a: 1 if s in rocks else 0
+      ret['R'].append(r)
 
-  ret['s0'] = (0, 0)
-  ret['psi'] = [1.0 / rewardCandNum] * rewardCandNum]
+  ret['s0'] = (size / 2, 0)
+  ret['psi'] = [1.0 / rewardCandNum] * rewardCandNum
+
+  return ret
--- lp.py
+++ lp.py
@@ -15,9 +15,10 @@ def lp(S, A, R, T, s0, psi, maxV):
   # useful constants
   rLen = len(R)
   M = 10000 # a large number
+  SA = iprod(S, A)
 
   # decision variables
-  x = var(xrange(len(S) * len(A)), 'x', bounds=(0, None))
+  x = var(SA, 'x', bounds=(0, None))
   z = var(xrange(rLen), 'z', bool)
   
   y = var(xrange(rLen))
@@ -26,22 +27,47 @@ def lp(S, A, R, T, s0, psi, maxV):
   minimize(sum([psi[i] * y[i] for i in xrange(rLen)]))
   
   # constraints on y
-  st([y[i] <= sum([x(s, a) * R[i](s, a) for s in S for a in A]) - maxV[i] + z[i] * M for i in xrange(rLen)])
+  st([y[i] <= sum([x[s, a] * R[i](s, a) for s in S for a in A]) - maxV[i] + z[i] * M for i in xrange(rLen)])
   st([y[i] <= (1 - z[i]) * M for i in xrange(rLen)])
   
   # constraints on x (valid occupancy)
   for sp in S:
     if sp == s0:
-      st(sum([x(sp, ap) for ap in A]) == 1)
+      st(sum([x[sp, ap] for ap in A]) == 1)
     else:
-      st(sum([x(sp, ap) for ap in A]) == sum([x(s, a) * T(s, a, sp) for s in S for a in A]))
+      st(sum([x[sp, ap] for ap in A]) == sum([x[s, a] * T(s, a, sp) for s in S for a in A]))
+  
+  solve()
+  print "Solver status:", status()
+  print 'Obj =', vobj()
+  
+  endModel()
+  return x, z
+
+def computeValue(pi, r, S, A):
+  sum = 0
+  for s in S:
+    for a in A:
+      sum += pi[s, a].value * r(s, a)
+  return sum
 
 if __name__ == '__main__':
-  # pass domain here
-  args = easyDomains.getRockDomain(10, 5, 10)
+  size = 10
+  numRocks = 1
+  rewardCandNum = 4
+  args = easyDomains.getRockDomain(size, numRocks, rewardCandNum, fixedRocks=True)
+  k = 4 # number of responses
   
-  for i in range(k):
-    if i == 0: args['maxV'] = 0
-    else: args['maxV'] = 
+  q = [] # query set
 
-    lp(**args)
+  for i in range(k):
+    if i == 0:
+      args['maxV'] = [0] * rewardCandNum
+    else:
+      # find the optimal policy so far that achieves the best on each reward candidate
+      args['maxV'] = []
+      for rewardId in xrange(rewardCandNum):
+        args['maxV'].append(max([computeValue(pi, args['R'][rewardId], args['S'], args['A']) for pi in q]))
+
+    (x, z) = lp(**args)
+    q.append(x)
